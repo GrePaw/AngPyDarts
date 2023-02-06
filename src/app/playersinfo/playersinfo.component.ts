b75a338f65f685765c3247aca06a54c5d912f97f
GrePaw <grepaw@outlook.de>
2023-02-06 20:15:43 +0100

    Input field on playersinfo displayed within html template

--- src/app/playersinfo/playersinfo.component.ts
+++ src/app/playersinfo/playersinfo.component.ts
@@ -1,26 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
+
 
 @Component({
   selector: 'app-playersinfo',
   templateUrl: './playersinfo.component.html',
   styleUrls: ['./playersinfo.component.css']
 })
-export class PlayersinfoComponent implements OnInit {
-  
-  value:any;
-  
-  constructor() { 
-    
+export class PlayersinfoComponent {
+  currentvalue="";
+  counter:number = 1;
+  playernames:string[]=[];
+
+  getVal(val: string) {
+    this.currentvalue = val
+    this.playernames = this.playernames.concat(val)
+    this.counter = this.counter +1
   }
-
-  ngOnInit(): void {
-  }
-
-  getName(){
-    console.log("Name entered is: " + this.value);
-  }
-
-
 
 }
+
 
