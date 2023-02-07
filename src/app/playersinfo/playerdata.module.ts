278af002a8af825ef6443e6eb78f09d918dd00de
GrePaw <grepaw@outlook.de>
2023-02-07 19:18:51 +0100

    Manual and button based score entry added + display in scoresheet

--- src/app/playersinfo/playerdata.module.ts
+++ src/app/playersinfo/playerdata.module.ts
@@ -5,14 +5,20 @@ export class Playerdata {
     prevthrow:number;
     lastav:number;
     longav:number;
+    activeP:boolean;
+
+
   
-    constructor(pn:string, curv:number, cura:number, pret:number, laav:number, loav:number ){
+    constructor(pn:string, curv:number, cura:number, pret:number, laav:number, loav:number,aP:boolean ){
       this.playername = pn;
       this.curval = curv;
       this.curav =cura;
       this.prevthrow = pret;
       this.lastav = laav,
       this.longav = loav;
+      this.activeP = aP;
     }
+
+
 }
 
--- src/app/playersinfo/playersinfo.component.ts
+++ src/app/playersinfo/playersinfo.component.ts
@@ -8,24 +8,51 @@ import { Playerdata } from './playerdata.module';
   styleUrls: ['./playersinfo.component.css']
 })
 export class PlayersinfoComponent {
+parseInt(arg0: string): number {
+throw new Error('Method not implemented.');
+}
   currentvalue="";
   counter:number = 0;
   playernames:string[]=[];
   playernames2:Playerdata[]=[];
   alldonevariable:Boolean = false;
+  activePnumber:number =0;
   
 
   getVal(val: string) {
     this.currentvalue = val
     this.playernames = this.playernames.concat(val)
-    this.playernames2[this.counter] = new Playerdata(val,301,0 ,0 ,0 ,0 )
+    this.playernames2[this.counter] = new Playerdata(val,301,0 ,0 ,0 ,0,false )
     this.counter = this.counter +1
+    console.log(this.counter)
+  }
+
+  addScore(score: any, objectlist:Playerdata[]) {
+    this.addEntry(score, objectlist)
   }
 
   allDone(){
     this.alldonevariable = true
   }
 
+  addEntry(entryval:number,  objectlist:Playerdata[]) {
+      
+    objectlist[this.activePnumber].curval = objectlist[this.activePnumber].curval - entryval;
+
+    this.activePnumber = this.activePnumber +1;
+
+    if (this.activePnumber > (objectlist.length -1)){
+      this.activePnumber = 0
+    }
+   }
+
+  skipPlayer(objectlist:Playerdata[]){
+    this.activePnumber = this.activePnumber +1;
+
+    if (this.activePnumber > (objectlist.length -1)){
+      this.activePnumber = 0
+    }
+   }
 }
 
 
